68ca560a2594133fafe75c3ed3cddc826731d296
kieranbrowne <kierantbrowne@gmail.com>
2014-06-04 01:32:30 +1000

    run.sh accepts integer arg for looping

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-cd bin/data/inspiration
-avconv -r 1/5 -i *.jpg -c:v libx264 ins.mp4
-cd ../../../
-make
-make run
+
+
+for ((n=0;n<${1:-${1:-1}};n++)); do
+
+#cd bin/data/inspiration
+#avconv -r 1/5 -i *.jpg -c:v libx264 ins.mp4
+#cd ../../../
+make;
+make run;
 #scp bin/data/$img to webserver
-cat bin/log.csv
-python python/newPrint.py
-img=`ls -1 bin/data | tail -1`
-display `echo bin/data/$img`
+cat bin/log.csv;
+python python/newPrint.py;
+#img=`ls -1 bin/data | tail -1`
+#display `echo bin/data/$img`
+
+done
